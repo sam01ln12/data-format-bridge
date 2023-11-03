06d28bc5b08204531479f24b620b856317971042
Samad Mirzayev <samad.mirzayev@kapitalbank.az>
2023-11-03 12:16:45 +0400

    Added initial dependencies for project

---
+++ data-format-bridge/src/main/java/com/format/bridge/dataformatbridge/DataFormatBridgeApplication.java
@@ -0,0 +1,13 @@
+package com.format.bridge.dataformatbridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataFormatBridgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataFormatBridgeApplication.class, args);
+    }
+
+}
